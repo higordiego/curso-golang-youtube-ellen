0b2efce9c28236af8e019daabe772cded786ed73
higordiego <higordiegoti@gmail.com>
2020-05-30 15:30:02 -0300

    add: exercicio 09

---
+++ nivel_3/ex09/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	esport := "futebol"
+
+	switch esport {
+	case "basquete":
+		fmt.Println("basquete")
+	case "futebol":
+		fmt.Println("futebol")
+	}
+}
