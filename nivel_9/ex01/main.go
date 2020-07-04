d86e5f576054d1bcd5fac91df8a10821ed1b4973
higordiego <higordiegoti@gmail.com>
2020-07-04 11:39:20 -0300

    add: exercicio

--- nivel_9/ex01/main.go
+++ nivel_9/ex01/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
 func main() {
-	
+
+	wg.Add(2)
+
+	go func() {
+		fmt.Println("Primeira goruntime")
+		wg.Done()
+	}()
+
+	go func() {
+		fmt.Println("Segunda goruntime")
+		wg.Done()
+	}()
+
+	wg.Wait()
 }
