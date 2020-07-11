c87e4f74f2ab277eee9062bb6edb3956ae577447
higordiego <higordiegoti@gmail.com>
2020-07-11 11:14:10 -0300

    add: exercicios

---
+++ nivel_10/ex06/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	c := make(chan int)
+
+	go func() {
+		for i := 0; i < 100; i++ {
+			c <- i
+		}
+		close(c)
+	}()
+	receive(c)
+
+}
+
+func receive(c chan int) {
+	for v := range c {
+		fmt.Println(v)
+	}
+
+}
